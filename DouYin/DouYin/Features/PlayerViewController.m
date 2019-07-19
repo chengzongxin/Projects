65ebed2451f328d587d5c78a98eb30c1664bdced
Joe <8876142@qq.com>
2019-07-19 16:04:05 +0800

    update

--- DouYin/DouYin/Features/PlayerViewController.m
+++ DouYin/DouYin/Features/PlayerViewController.m
@@ -11,6 +11,7 @@
 
 @interface PlayerViewController ()<AVPlayerViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UISlider *slider;
 @property (weak, nonatomic) IBOutlet UILabel *currentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (strong, nonatomic) AVPlayerView *playerView;
@@ -24,7 +25,8 @@
     self.view.backgroundColor = UIColor.whiteColor;
     AVPlayerView *playerView = [[AVPlayerView alloc] initWithFrame:self.view.bounds];
     playerView.delegate = self;
-    [self.view addSubview:playerView];
+    [self.view insertSubview:playerView atIndex:0];
+    
     // 10
     [playerView setPlayerUrl:@"https://gss3.baidu.com/6LZ0ej3k1Qd3ote6lo7D0j9wehsv/tieba-smallvideo/607272_373beb1043e8dae94026e937085934d0.mp4"];
     
@@ -38,8 +40,24 @@
 }
 
 - (void)onProgressUpdate:(CGFloat)current total:(CGFloat)total{
-    self.currentLabel.text = @(current).stringValue;
-    self.totalLabel.text = @(total).stringValue;
+    self.currentLabel.text = [self unitTrans:current];
+    self.totalLabel.text = [self unitTrans:total];
+    self.slider.maximumValue = total;
+}
+- (IBAction)sliderChange:(UISlider *)slider {
+    [self.playerView seekToProgress:slider.value];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.playerView updatePlayerState];
+}
+
+- (NSString *)unitTrans:(float)value{
+    if (value < 60) {
+        return [NSString stringWithFormat:@"00:%02f",ceilf(value)];
+    }else {
+        return [NSString stringWithFormat:@"%02f:%02f",ceilf(value) / 60,(float)((int)value % 60)];
+    }
 }
 
 @end
