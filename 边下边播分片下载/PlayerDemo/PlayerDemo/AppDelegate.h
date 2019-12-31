28422cfdc2cbabc8bba38b415a1cae7b90b1d02e
Joe <8876142@qq.com>
2019-12-31 18:10:58 +0800

    add 边下边播demo

---
+++ 边下边播分片下载/PlayerDemo/PlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayerDemo
+//
+//  Created by Joe on 2019/12/31.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
