270bb6eec4702bc80118d0425ef3e9119171ece3
Joe <8876142@qq.com>
2020-02-17 17:48:45 +0800

    update

--- HomeCycleViewDemo/HomeCycleViewDemo/HomeCycleView/HomeCycleIndexCell.m
+++ HomeCycleViewDemo/HomeCycleViewDemo/HomeCycleView/HomeCycleIndexCell.m
@@ -7,12 +7,73 @@
 //
 
 #import "HomeCycleIndexCell.h"
+#import "IndexListCell.h"
+
+@interface HomeCycleIndexCell ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+
+@property (nonatomic,strong) UICollectionView *collectionView;
+@property (nonatomic,strong) UICollectionViewFlowLayout *layout;
+
+@end
 
 @implementation HomeCycleIndexCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
+    
+    [self addSubview:self.collectionView];
+}
+
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    
+    self.collectionView.frame = CGRectMake(0, 36, self.bounds.size.width, self.bounds.size.height - 36);
+    
+    _layout.itemSize = CGSizeMake((self.collectionView.frame.size.width - _layout.minimumInteritemSpacing*3) / 3, (self.collectionView.frame.size.height - _layout.minimumInteritemSpacing*2) / 2);
+}
+
+#pragma mark UICollectionView Delegate
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 12;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    IndexListCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass(IndexListCell.class) forIndexPath:indexPath];
+    return cell;
+}
+
+#pragma mark - Getter
+- (UICollectionView *)collectionView{
+    if (!_collectionView) {
+        //1.初始化layout
+        UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+        //设置collectionView滚动方向
+        //不滚动
+        [layout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+        //设置headerView的尺寸大小
+        //该方法也可以设置itemSize
+        layout.itemSize = self.bounds.size;
+        layout.minimumInteritemSpacing = 7;
+        layout.minimumLineSpacing = 7;
+//        layout.sectionInset = UIEdgeInsetsMake(0, 7, 0, 7);
+        _layout = layout;
+        
+        //2.初始化collectionView
+        _collectionView = [[UICollectionView alloc] initWithFrame:self.bounds collectionViewLayout:layout];
+        _collectionView.backgroundColor = UIColor.clearColor;
+        _collectionView.pagingEnabled = YES;
+        //        _collectionView.contentInset = UIEdgeInsetsMake(0, kCellMargin, 0, kCellMargin);
+        //3.注册collectionViewCell
+        //注意，此处的ReuseIdentifier 必须和 cellForItemAtIndexPath 方法中 一致 均为 cellId
+        [_collectionView registerNib:[UINib nibWithNibName:NSStringFromClass(IndexListCell.class) bundle:nil] forCellWithReuseIdentifier:NSStringFromClass(IndexListCell.class)];
+        
+        //4.设置代理
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+    }
+    return _collectionView;
 }
 
 @end
