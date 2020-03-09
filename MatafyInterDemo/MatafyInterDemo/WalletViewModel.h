deca43545ccf7d22d182324a11017dc45d9e9b52
Joe <8876142@qq.com>
2020-03-09 19:37:31 +0800

    接口API添加 小驼峰转换工具

---
+++ MatafyInterDemo/MatafyInterDemo/WalletViewModel.h
@@ -0,0 +1,17 @@
+//
+//  WalletViewModel.h
+//  MatafyInterDemo
+//
+//  Created by Joe on 2020/3/9.
+//  Copyright © 2020年 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WalletViewModel : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MatafyInterDemo/MatafyInterDemo/WalletViewModel.m
@@ -0,0 +1,21 @@
+//
+//  WalletViewModel.m
+//  MatafyInterDemo
+//
+//  Created by Joe on 2020/3/9.
+//  Copyright © 2020年 Joe. All rights reserved.
+//
+
+#import "WalletViewModel.h"
+
+@implementation WalletViewModel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
