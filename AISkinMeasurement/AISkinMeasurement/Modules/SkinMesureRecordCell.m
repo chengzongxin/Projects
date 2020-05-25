f9c4b5f2d2bcd72f1751d9eb2254d7fe5b81e97a
Joe <8876142@qq.com>
2020-05-25 16:34:37 +0800

    update

--- AISkinMeasurement/AISkinMeasurement/Modules/SkinMesureRecordCell.m
+++ AISkinMeasurement/AISkinMeasurement/Modules/SkinMesureRecordCell.m
@@ -16,7 +16,7 @@
     
     self.layer.cornerRadius = 8;
     
-    _scoreTagButtonLeft = [[UIButton alloc] initWithFrame:CGRectMake(62,18,107,18)];
+    _scoreTagButtonLeft = [[UIButton alloc] initWithFrame:CGRectMake(62,18,97,18)];
     _scoreTagButtonLeft.backgroundColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0];
     _scoreTagButtonLeft.layer.cornerRadius = 4;
     _scoreTagButtonLeft.layer.borderWidth = 0.5;
@@ -24,9 +24,9 @@
     _scoreTagButtonLeft.layer.borderColor = [UIColor colorWithRed:0/255.0 green:195/255.0 blue:206/255.0 alpha:1.0].CGColor;
     [_scoreTagButtonLeft setTitleColor:rgba(0, 195, 206, 1) forState:UIControlStateNormal];
     [_scoreTagButtonLeft setTitle:@"肌肤良好指数" forState:UIControlStateNormal];
-    _scoreTagButtonLeft.titleLabel.font = [UIFont systemFontOfSize:10];
+    _scoreTagButtonLeft.titleLabel.font = [UIFont fontWithName:@"PingFangSC-Medium" size: 10];
     _scoreTagButtonLeft.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
-    _scoreTagButtonLeft.titleEdgeInsets = UIEdgeInsetsMake(0, 3, 0, 0);
+    _scoreTagButtonLeft.titleEdgeInsets = UIEdgeInsetsMake(0, 5, 0, 0);
     [self addSubview:_scoreTagButtonLeft];
     
     _scoreTagButtonRight = [[UIButton alloc] initWithFrame:CGRectMake(_scoreTagButtonLeft.bounds.size.width - 29,0,29,18)];
@@ -34,7 +34,7 @@
 //    _scoreTagButtonRight.layer.cornerRadius = 4;
     [_scoreTagButtonRight setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
     [_scoreTagButtonRight setTitle:@"良好" forState:UIControlStateNormal];
-    _scoreTagButtonRight.titleLabel.font = [UIFont systemFontOfSize:9];
+    _scoreTagButtonRight.titleLabel.font = [UIFont fontWithName:@"PingFangSC-Medium" size: 10];
     [_scoreTagButtonLeft addSubview:_scoreTagButtonRight];
     
     _checkButton.layer.cornerRadius = 10;
